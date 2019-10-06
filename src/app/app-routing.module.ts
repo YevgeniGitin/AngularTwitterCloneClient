2cbf4f56b7def9c578b357b9e854476aef64e97d
Yevgeni Gitin <yevgeni.gitin21@gmail.com>
2019-10-06 10:59:41 +0300

    modules commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProfileComponent } from './user-profile/components/profile/profile.component';
 
 const routes: Routes = [
-  {path:'profile/:userName',component:ProfileComponent, pathMatch:'full'},
   {path: '',redirectTo:"/home", pathMatch:"full" }
 ];
 
